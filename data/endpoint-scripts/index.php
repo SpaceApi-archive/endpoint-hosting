a42c8ee7cc07edf6cce07ad793e5e53e41b99f6a
slopjong <git@slopjong.de>
2014-02-21 13:04:22 +0100

    Updated the endpoint scripts to a928dd25e1

--- data/endpoint-scripts/index.php
+++ data/endpoint-scripts/index.php
@@ -16,16 +16,42 @@ switch(get_controller()) {
                 // is supported. The problem is the addressing of specific
                 // instances of a sensor array of the same type
 
-                if($key !== @$_POST['key'])
-                    die('Not allowed');
+                if( !isset($_POST['key']) && !isset($_GET['key']) )
+                {
+                    header('HTTP/1.0 403 Forbidden');
+                    die('No key provided');
+                }
 
-                $sensors = urldecode(@$_POST['sensors']);
+                $sensors = '';
+                $client_key = '';
+                switch($_SERVER['REQUEST_METHOD'])
+                {
+                    case 'POST':
+
+                        $client_key = $_POST['key'];
+                        $sensors = urldecode($_POST['sensors']);
+                        break;
+
+                    case 'GET':
+
+                        $client_key = $_GET['key'];
+                        $sensors = urldecode($_GET['sensors']);
+                        break;
+                }
+
+                if($client_key !== $key)
+                {
+                    header('HTTP/1.0 403 Forbidden');
+                    die('Wrong key');
+                }
 
                 // convert the json to an associative array
                 $sensors = json_decode($sensors, true);
 
                 if(! is_null($sensors))
                     save_sensors($sensors);
+                else
+                    die('Invalid JSON: '. $sensors);
 
                 break;
 
@@ -117,14 +143,22 @@ function get_router_segment($index) {
 function get_request_uri() {
 
     $request_uri = $_SERVER['REQUEST_URI'];
-    $request_uri = preg_replace('|^/|', '', $request_uri);
+    //$request_uri = preg_replace('|^/|', '', $request_uri);
 
     if($request_uri === '') {
         $request_uri = @$_SERVER['REDIRECT_URL'];
-        $request_uri = preg_replace('|^/|', '', $request_uri);
+        //$request_uri = preg_replace('|^/|', '', $request_uri);
     }
 
-    return $request_uri;
+    $route = str_replace(
+        __DIR__,
+        '',
+        $_SERVER['DOCUMENT_ROOT'] . $request_uri
+    );
+
+    $route = preg_replace('|^/|', '', $route);
+
+    return $route;
 }
 
 
@@ -166,47 +200,51 @@ function output_json() {
 
     // iterate over all sensor files and merge them with the static
     // spaceapi.json
-    foreach(glob('data/*') as $file) {
+    $sensor_data = glob('data/*');
 
-        $file_content = file_get_contents($file);
+    if  (!empty($sensor_data)) {
+        foreach(glob('data/*') as $file) {
 
-        list($type, $value) = explode(':', $file_content);
-        settype($value, $type);
+            $file_content = file_get_contents($file);
 
-        // here we take the file name of a data file (in the data dir)
-        // and create a list of indices which will be to address the
-        // corresponding field in the spaceapi.json template.
-        $array_path = basename($file);
-        $array_path = explode('.', $array_path);
+            list($type, $value) = explode(':', $file_content);
+            settype($value, $type);
 
-        // get a reference of the spaceapi, see the explanation later
-        $sub_array = &$spaceapi;
+            // here we take the file name of a data file (in the data dir)
+            // and create a list of indices which will be to address the
+            // corresponding field in the spaceapi.json template.
+            $array_path = basename($file);
+            $array_path = explode('.', $array_path);
 
-        $do_write_value = true;
-        foreach($array_path as $path_segment) {
+            // get a reference of the spaceapi, see the explanation later
+            $sub_array = &$spaceapi;
 
-            // here we check if the sensor (or what we pushed to the
-            // endpoint scripts) is defined in the template, if it's
-            // not we will skip the value. The skip is done via a flag
-            // since we cannot use continue because we'd need to
-            // tell the outer loop to continue but not the current one
-            // we're currently in
-            if(!array_key_exists($path_segment, $sub_array))
-            {
-                $do_write_value = false;
-                break;
+            $do_write_value = true;
+            foreach($array_path as $path_segment) {
+
+                // here we check if the sensor (or what we pushed to the
+                // endpoint scripts) is defined in the template, if it's
+                // not we will skip the value. The skip is done via a flag
+                // since we cannot use continue because we'd need to
+                // tell the outer loop to continue but not the current one
+                // we're currently in
+                if(!array_key_exists($path_segment, $sub_array))
+                {
+                    $do_write_value = false;
+                    break;
+                }
+
+                // get the sub array of the spaceapi data structure (taken
+                // from the template) while we walk along the path according
+                // the file name (sliced into indices)
+                $sub_array = &$sub_array[$path_segment];
             }
 
-            // get the sub array of the spaceapi data structure (taken
-            // from the template) while we walk along the path according
-            // the file name (sliced into indices)
-            $sub_array = &$sub_array[$path_segment];
+            // finally merge the value of the data file into the spaceapi
+            // data structure
+            if($do_write_value)
+                $sub_array = $value;
         }
-
-        // finally merge the value of the data file into the spaceapi
-        // data structure
-        if($do_write_value)
-            $sub_array = $value;
     }
 
     echo json_encode($spaceapi);
