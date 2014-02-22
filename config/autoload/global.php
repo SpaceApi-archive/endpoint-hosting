14398a2e4bfd693ba576821f0673d0dce158106a
slopjong <git@slopjong.de>
2014-02-22 14:15:49 +0100

    Simplified the config files

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,20 +1,13 @@
 <?php
-/**
- * Global Configuration Override
- *
- * You can use this file for overriding configuration values from modules, etc.
- * You would place values in here that are agnostic to the environment and not
- * sensitive to security.
- *
- * @NOTE: In practice, this file will typically be INCLUDED in your source
- * control, so do not include passwords or other sensitive information in this
- * file.
- */
 
+// don't change the values here, copy local.php.dist and remove the extension instead
 return array(
 
-    // To enable the logger, use logger.local.php
     'enabled_logger' => array(
         'error_log'  => false,
     ),
+
+    'emails' => array(),
+
+    'gist_token' => ''
 );
