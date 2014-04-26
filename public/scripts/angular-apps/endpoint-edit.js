860578bf584996bcc5828cab957df1c17a860254
slopjong <git@slopjong.de>
2014-04-27 00:47:24 +0200

    Made the json editor play nice with endpoint/index and endpoint/edit

--- public/scripts/angular-apps/endpoint-edit.js
+++ public/scripts/angular-apps/endpoint-edit.js
@@ -1,3 +1,14 @@
 'use strict';
 
-angular.module('EndpointEditApp', ['jsoneditor']);
+angular
+  .module('EndpointEditApp', ['jsoneditor'])
+  .controller('EndpointEditController', function($scope, $element) {
+
+    $scope.aceChanged = function($e) {
+      var json = $e[1].session.getValue();
+      var textarea = angular.element($element.find('textarea')[0]);
+      textarea.text(json);
+    };
+
+  });
+
